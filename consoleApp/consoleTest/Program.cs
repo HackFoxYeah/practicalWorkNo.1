7ac2c043e0e17e23d6bc7f314f262249afa08c37
Короче, Максон <102228515+HackFoxYeah@users.noreply.github.com>
2022-11-27 14:11:27 +0300

    firstCommit

---
+++ consoleApp/consoleTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace consoleTest;
+
+public record Cat(string Name, int Age)
+{
+    public string Name { get; set; } = Name;
+
+    public int Age { get; set; } = Age;
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 10; j++)
+                Console.WriteLine(i + j);
+
+        Console.WriteLine();
+    }
+}
